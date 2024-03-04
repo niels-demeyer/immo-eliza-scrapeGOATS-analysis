c7cb60dbeca88334036e7ae350a9fc0d44df2353
nelzon <niels.demeyer99@gmail.com>
2024-03-04 13:45:52 +0100

    streamlit run

--- analysis/app.py
+++ analysis/app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit import sidebar
 import geopandas as gpd
 import pandas as pd
 import os
@@ -12,7 +11,7 @@ def load_houses_data_pandas():
     rel_path = "../data/raw/houses_cleaned.csv"
     file_path = os.path.join(script_dir, rel_path)
     data = pd.read_csv(file_path)
-    data['city'] = data['city'].str.lower()  # convert city names to lowercase
+    data['city'] = data['city'].str.lower().str.strip()  # convert city names to lowercase and remove whitespaces
     return data
 
 # Load the geojson file
@@ -21,7 +20,7 @@ def load_geojson():
     rel_path = "../data/raw/BELGIUM_-_Municipalities.geojson"
     file_path = os.path.join(script_dir, rel_path)
     data = gpd.read_file(file_path)
-    data['Communes'] = data['Communes'].str.lower()  # convert city names to lowercase
+    data['Communes'] = data['Communes'].str.lower().str.strip()  # convert city names to lowercase and remove whitespaces
     return data
 
 def calculate_average_price(data, group_by_column, agg_column):
@@ -29,17 +28,15 @@ def calculate_average_price(data, group_by_column, agg_column):
 
 def plot_most_expensive_houses_average(data, geojson):
     avg_price = calculate_average_price(data, 'city', 'price')
-    plot_map(avg_price, geojson, 'city', "properties.NAME_4", 'price')
-
-
+    plot_map(avg_price, geojson, 'city', "properties.Communes", 'price')
 
 def plot_map(avg_price, geojson, locations, featureidkey, color):
-    fig = px.choropleth_mapbox(avg_price, geojson=geojson, locations=locations, featureidkey=featureidkey,
+    fig = px.choropleth_mapbox(avg_price, geojson=geojson.__geo_interface__, locations=locations, featureidkey=featureidkey,
                                color=color,
-                               color_continuous_scale="Viridis",  # use the Viridis color scale
+                               color_continuous_scale="cividis",  # use the Cividis color scale
                                range_color=(avg_price[color].min(), avg_price[color].max()),  # adjust the legend based on the max average price
                                mapbox_style="carto-positron",
-                               zoom=6, center = {"lat": 50.8503, "lon": 4.3517},
+                               zoom=5, center = {"lat": 50.8503, "lon": 4.3517},
                                opacity=0.5,
                                labels={'price':'Average price per municipality'}
                               )
@@ -63,7 +60,6 @@ def streamlit_app():
     if st.sidebar.checkbox('Show raw data'):
         st.subheader('Raw Data')
         st.write(houses_data)
-
 
 def main():
     streamlit_app()
